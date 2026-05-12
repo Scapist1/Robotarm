253251d1fdfb9f9ee8f89120631d81e8092347c2
Scapist1 <jaco6jaco6@gmail.com>
2026-05-12 17:25:50 +0200

    timer ændring

--- include/timer.h
+++ include/timer.h
@@ -3,9 +3,6 @@
 
 #include <avr/io.h>
 
-// TCNT1 (Timer 1) tæller op konstant, OCR1A er slutværdien der sender TCNT1 tilbage til 0
-// Vi bruger Timer 1 (16-bits), da det er mere præcist desto flere bits. Den har 65.535 ticks. En 8-bit har kun 255 ticks. stor forskel!
-
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
